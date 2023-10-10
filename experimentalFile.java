4c747e30a73970c0c36f8d50e15934cbfb71a5eb
queenie-lee <queenie.lee@live.ca>
2023-10-10 18:15:59 +0100

    Added text from new branch

---
+++ experimentalFile.java
@@ -0,0 +1,6 @@
+public class experimentalFile {
+    public static void main(String[] args) {
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+        System.out.println("New branch change");
+    }
+}
